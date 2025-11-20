a01c31d8a0e9db5037519761ad73c0071c6c5a98
sumit <susingh@veneklasen.com>
2025-11-20 07:31:53 +0000

    made some lintng error to check CI is working or not

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 import Image from "next/image";
 
 export default function Home() {
+  const unused = "hello"; // ❌ Lint error: 'unused' is assigned but never used
+
   return (
-     <div className="min-h-screen flex justify-center items-center bg-white">
-        <Image
-          src="/images/img_01.avif"
-          alt="huiuiui"
-          width={800}
-          height={600}
-          className="w-fit h-fit rounded-lg shadow-lg"
-          />
+    <div className="min-h-screen flex justify-center items-center bg-white">
+      <Image
+        src="/images/img_01.avif"
+        alt="huiuiui"
+        width={800}
+        height={600}
+        className="w-fit h-fit rounded-lg shadow-lg"
+      />
     </div>
   );
 }
