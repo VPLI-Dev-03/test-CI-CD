af2fd51d033adf4a117199f5c6e8d855c8515f33
sumit <susingh@veneklasen.com>
2025-12-02 04:42:37 +0000

    make linting error 11

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,23 @@
-"use client"
-import Image from "next/image";
+"use client";
 import { useState } from "react";
+import Image from "next/image";
 
 export default function Home() {
+  const x = 23;
+  console.log(notDefined);
 
   return (
     <div className="min-h-screen flex justify-evenly items-center bg-white">
-      <Image
+      <img
         src="/images/img_01.avif"
         alt="huiuiui"
         width={800}
         height={600}
         className="w-fit h-fit rounded-lg shadow-lg"
       />
-      <h1 className="font-bold text-3xl text-red-800">Welcome to the Home Page</h1>
+      <h1 className="font-bold text-3xl text-red-800">
+        Welcome to the Home Pageee
+      </h1>
     </div>
   );
 }
